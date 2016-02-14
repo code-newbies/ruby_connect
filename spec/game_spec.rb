af56138c74787783252d9a633377a9c28e435d34
Ivan Lasorsa <ivan.lasorsa@gmail.com>
2016-02-14 13:37:20 +0100

    Change spec for an empty board to use the Board class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,19 @@
 require 'game'
 
-describe Game do
-  let(:game) { Game.new }
+module RubyConnect
+  describe Game do
+    let(:game) { Game.new }
 
-  it 'has an empty board' do
-    expect(game.board).to have_attributes(size: 7)
-  end
+    it 'has an empty board' do
+      expect(game.board.is_empty?).to be true
+    end
 
-  it 'can add one piece to the board'
-  it 'switches player turns after one move'
+    it 'can add one piece to the board'
+    it 'switches player turns after one move'
 
-  describe '#winning_move?' do
-    it 'in a vertical direction'
-    it 'in a horizontal direction'
+    describe '#winning_move?' do
+      it 'in a vertical direction'
+      it 'in a horizontal direction'
+    end
   end
 end
