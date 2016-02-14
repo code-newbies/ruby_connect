477adc6b567d424ffc83b98f101297eb3fe151a8
Ivan Lasorsa <ivan.lasorsa@gmail.com>
2016-02-14 13:35:47 +0100

    Draft specs for Board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,40 @@
+require 'board'
+
+module RubyConnect
+  describe Board do
+    let(:board) { subject }
+
+    context 'when a move is still not done' do
+      it 'is empty' do
+        expect(board.is_empty?).to be true
+      end
+    end
+
+    context 'when a move is done' do
+      before { board.insert_into_column 1, :red }
+
+      it 'is not empty' do
+        expect(board.is_empty?).to be false
+      end
+    end
+
+    context 'when all moves are done' do
+      let(:full_board) do
+        (1..Board::COLUMNS).each do |column|
+          (1..Board::ROWS).each do |row|
+            board.insert_into_column column, :red
+          end
+        end
+        board
+      end
+
+      it 'is full' do
+        expect(full_board.is_full?).to be true
+      end
+
+      it 'raises an error if you try to make a move' do
+        expect{ full_board.insert_into_column 1, :red }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
