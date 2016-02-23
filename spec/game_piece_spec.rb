c7d3631e663f5d87caf1a8e8d1ce7e6fd108388d
ashleymichal <ashleymichal@gmail.com>
2016-02-22 18:24:34 -0600

    add tests for GamePiece class

---
+++ spec/game_piece_spec.rb
@@ -0,0 +1,14 @@
+require 'game_piece'
+
+module RubyConnect
+  describe GamePiece do
+    let(:game_piece) { subject }
+
+    describe 'a player move' do
+      context 'when a player selects a column' do
+        it ''
+      end
+    end
+
+  end
+end
